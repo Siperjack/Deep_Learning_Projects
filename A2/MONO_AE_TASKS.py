eb58ad756ac5406ae7b56c15e212a1df09f6395a
Siperjack <62109894+Siperjack@users.noreply.github.com>
2022-03-08 13:43:49 +0100

    Completed anomaly detection with printing to screen of the 25 most anomalious ones based on reconstruction error with RMS error. Results are fat numbers and a lot of the missing number 8. So it seems fine.

---
+++ A2/MONO_AE_TASKS.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras import layers, models
+from stacked_mnist import StackedMNISTData, DataMode
+import os
+import matplotlib.pyplot as plt
+from verification_net import VerificationNet
+from class_AE import AE
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+#%%Global constants
+n_dim = 28
+latent_dim = 2
+Nchannels = 1
+trained = False
+epochs = 2
+#%% Loading
+AE_MONO_BINARY_COMPLETE = AE(trained = trained, filename = "./models/MONO_BINARY_COMPLETE")
+
+data = StackedMNISTData(mode=DataMode.MONO_BINARY_COMPLETE, default_batch_size=2048)
+train_images, train_labels = data.get_full_data_set(training = True)
+val_images, val_labels = data.get_full_data_set(training = False)
+AE_MONO_BINARY_COMPLETE.train(train_images, val_images, epochs = epochs)
+
+#%% Make reconstructions
+AutoEncoder = AE_MONO_BINARY_COMPLETE.AutoEncoder
+reconstructed_images = AutoEncoder.predict(val_images)
+#%% Plot example reconstructed
+n = 8
+fig, ax = plt.subplots(2,n)
+for i in range(n):
+    plt.gray()
+    ax[0,i].imshow(val_images[i].reshape((n_dim, n_dim)))
+    ax[1,i].imshow(reconstructed_images[i].reshape((n_dim, n_dim)))
+fig, ax = plt.subplots(2,n)
+for i in range(n):
+    plt.gray()
+    ax[0,i].imshow(val_images[n+i].reshape((n_dim, n_dim)))
+    ax[1,i].imshow(reconstructed_images[n+i].reshape((n_dim, n_dim)))        
+        
+    
+
+#%%Generative model
+Ngen = 2048*4
+z = np.random.uniform(0,1, (Ngen, latent_dim, Nchannels))
+
+uniform_generated = AE_MONO_BINARY_COMPLETE.Decoder.predict(z)
+
+n = 5
+fig, ax = plt.subplots(n,n)
+for i in range(n):
+    for j in range(n):
+        plt.gray()
+        ax[i,j].imshow(uniform_generated[i + n*j].reshape((n_dim, n_dim)))
+
+#%% Running verification net on reconstructed images
+gen = StackedMNISTData(mode=DataMode.MONO_BINARY_COMPLETE, default_batch_size=2048)
+net = VerificationNet(force_learn=False)
+net.train(generator=gen, epochs=5)
+
+
+cov = net.check_class_coverage(data=reconstructed_images, tolerance=.8)
+pred, acc = net.check_predictability(data=reconstructed_images, correct_labels=val_labels)
+print(f"Coverage: {100*cov:.2f}%")
+print(f"Predictability: {100*pred:.2f}%")
+print(f"Accuracy: {100 * acc:.2f}%")    
+    
+#%%Anomalies
+
+AE_MONO_BINARY_MISSING = AE(trained = trained, filename = "./models/MONO_BINARY_MISSING")
+AutoEncoder_missing = AE_MONO_BINARY_MISSING.AutoEncoder
+
+
+data_missing = StackedMNISTData(mode=DataMode.MONO_BINARY_MISSING, default_batch_size=2048)
+train_images_missing, train_labels_missing = data.get_full_data_set(training = True)
+val_images_missing, val_labels_missing = data.get_full_data_set(training = False)
+
+AE_MONO_BINARY_MISSING.train(train_images_missing, val_images_missing,epochs = epochs)
+#%%
+results = AutoEncoder_missing.predict(val_images)
+#%%
+
+losses = tf.reduce_mean(results - val_images, axis = [1,2,3])
+losses_indexes = np.argsort(losses)
+
+#%% Plot example reconstructed with highest errors
+
+n = 5
+fig, ax = plt.subplots(n,n)
+for i in range(n):
+    for j in range(n):
+        plt.gray()
+        ax[i,j].imshow(val_images[losses_indexes[i + n*j]].reshape((n_dim, n_dim)))
+
+
+
+
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
--- A2/class_VAE.py
+++ A2/class_VAE.py
@@ -11,22 +11,27 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 #Architecture
    
-class AE:
+class VAE:
     def __init__(self, filename, trained = True):
         self.n_dim = 28
         self.latent_dim = 2
+        self.Nchannels = Nchannels
         self.filename = filename
         self.trained = trained
         
         """Encoder"""
         
         model = models.Sequential(name='Encoder')
-        model.add(layers.Conv2D(64, (3,3),strides = 1, padding = "same", activation = "relu", input_shape = (self.n_dim, self.n_dim, 1)))
+        model.add(layers.Conv2D(64, (3,3),strides = 1, padding = "same", activation = "relu", input_shape = (self.n_dim, self.n_dim, Nchannels)))
+        model.add(layers.MaxPooling2D(pool_size = (2,2)))
+        model.add(layers.Dropout(0.25))
+        model.add(layers.Conv2D(64, (3,3),strides = 1, padding = "same", activation = "relu", input_shape = (self.n_dim, self.n_dim, Nchannels)))
         model.add(layers.MaxPooling2D(pool_size = (2,2)))
         model.add(layers.Dropout(0.25))
         model.add(layers.Conv2D(32, (3,3),strides = 2, padding = "same",activation = "relu"))
         model.add(layers.MaxPooling2D(pool_size = (2,2)))
         model.add(layers.Dropout(0.25))
+        
         model.add(layers.Flatten())
         model.add(layers.Dense(self.latent_dim))
         model.add(layers.Normalization())
@@ -36,13 +41,13 @@ class AE:
         """Decoder"""
         
         Decoder = models.Sequential(name='Decoder')
-        Decoder.add(layers.Dense(1568, activation = "relu", input_shape = (self.latent_dim, 1)))
+        Decoder.add(layers.Dense(1568, activation = "relu", input_shape = (self.latent_dim, self.Nchannels)))
         Decoder.add(layers.Reshape((7, 7, 32*self.latent_dim)))
         Decoder.add(layers.Conv2DTranspose(32, (4,4), strides = 2,padding = "same", activation = "relu"))
         Decoder.add(layers.Conv2DTranspose(32, (4,4), strides = 1,padding = "same", activation = "relu"))
-        Decoder.add(layers.Conv2DTranspose(1, (4,4), strides = 2,padding = "same", activation = "sigmoid"))
+        Decoder.add(layers.Conv2DTranspose(Nchannels, (4,4), strides = 2,padding = "same", activation = "sigmoid"))
         
-        """Compiling Encoder + Decoder"""
+        """Encoder + Decoder"""
         
         Encoder_output = Encoder.output
         out = Decoder(Encoder_output)
@@ -62,18 +67,22 @@ class AE:
             print("weights loaded")
 
     
-    def train(self, train_images, val_images, batch_size = 1024, epochs = 5):
+    def train(self, train_images, val_images, batch_size = 1024):
         if not self.trained:
             self.AutoEncoder.fit(
                 train_images,
                 train_images,#label
-                epochs = epochs,
+                epochs = 1,
                 shuffle = True,
-                batch_size = batch_size,
+                batch_size = C,
                 validation_data=(val_images, val_images)
                 )
             
-            self.AutoEncoder.save_weights(self.filename)
+            AutoEncoder.save_weights(self.filename)
             print("model is trained and weights saved")
         else:
             print("model is already trained")
+
+        
+
+    
