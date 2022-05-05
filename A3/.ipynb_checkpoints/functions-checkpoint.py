0d4778afb5e0fb9080845fd7b6aa2ad2555f482c
siperjack <jo.stokke.andersson@gmail.com>
2022-05-05 09:55:30 +0000

    fixed alt forecast with flow added, and made some models and folders

--- A3/.ipynb_checkpoints/functions-checkpoint.py
+++ A3/.ipynb_checkpoints/functions-checkpoint.py
@@ -17,33 +17,230 @@ from ClassRNN import RNN
 import matplotlib.pyplot as plt
 
 
-def n_in_1_out(data, model, pred_window): #data is assumed to be of the [batch, n_seq, n_feature] format
-    start_ind = len(data) - pred_window
-    model_input = data[[start_ind]]
-    forecasts = []
-    forecast = model.predict(model_input)[0,-1]
-    forecasts.append(forecast)
-    for i in range(pred_window - 1):
-        model_input = data[[start_ind + 1 + i]]
-        #print(model_input.shape)
-        model_input[0, -1, -1] = forecasts[-1] #Last prev_y datapoint is replaced by last prediction
-        forecast = model.predict(model_input)[0,-1]
-        forecasts.append(forecast)
-    return np.array(forecasts)
+def smooth_third_deviation(y): #assumes burstes are distributet randomly and two are not found in a row
+    E = np.sum(y)/len(y)
+    var = np.sum((y - E)**2)
+    for i in range(len(y)):
+        if y[i] > E + 3*np.sqrt(var) or y[i] < E - 3*np.sqrt(var):
+            if i == 0:
+                y[0] = y[1]
+            elif i == len(y)-1:
+                y[-1] = y[-2]
+            else:
+                y[i] = E
+    return y
+            
+def smooth_filter(y): #assumes burstes are distributet randomly and two are not found in a row
+    count = 0
+    for i in range(len(y)):
+        if i < 5: 
+            zum = (np.sum(y[0:i+5])  - y[i])/ (6 + i)
+        elif i > len(y) - 5:
+            zum = (np.sum(y[i-5:-1]) - y[i]) / (6 + len(y) - i)
+        else:
+            zum = (np.sum(y[i-5:i+5]) - y[i]) / 11
+        if abs(y[i]) > 2*abs(zum):
+            y[i] = zum
+            count += 1
+    print(f"{count/len(y)} datapoints smoothed in clamming")
+    return y
 
-def create_dataset(data, n_seq):
-    x_data = np.asarray(data.values[:,1:-1]).astype(np.float32) #(225089, 8)
-    y_data = np.asarray(data.values[:,[-1]]).astype(np.float32) #(225089, 1)
-    x_data = np.concatenate((x_data[1:], y_data[:-1]), axis = 1) #Data in timestep t_i+1 uses y from t_i
-    y_data = y_data[:-1]
-    x_data, x_norm = sklearn.preprocessing.normalize(x_data)
-    y_data, y_norm = sklearn.preprocessing.normalize(y_data)
-    
-    x_list, y_list = [], []
-    print(len(x_data))
-    for i in range(len(x_data) - n_seq - 1):
-        x_list.append(x_data[i:(i+n_seq)])
-        y_list.append(y_data[i:(i+n_seq)])
+
+def add_lag_features(data, mean = False, all_lags = True, naive = False):
+    if not naive:
+        # print(data.head())
+        data["y_prev"] = data["y"].shift(1)
+        # print(data.head())
+        if all_lags:
+            data["y_prev_day"] = data["y"].shift(12*24)
+            # print(data.head())
+            data["y_prev_week"] = data["y"].shift(12*24*7)
+            # print(data.head())
+            data = data.drop(data.index[0:12*24*7])
+            # print(data.head())
+        else:
+            data = data.drop(data.index[0])
+
         
-    print(len(x_list))
-    return np.array(x_list), np.array(y_list), x_norm, y_norm 
+    
+    
+    
+    y_index = data.columns.get_loc("y")
+    # print(y_index)
+    cols = data.columns.tolist()
+    # print(cols)
+    if y_index == len(data):
+        print("y feature already in last column")
+    else:
+        cols = cols[:y_index] + cols[y_index + 1:] + cols[y_index:y_index + 1]
+        # print(cols)
+        data = data[cols]
+        print("final head after adding features are \n" , data.head())
+        if naive:
+            print("naive model called")
+    
+    
+    return data
+
+def add_time_features(data):
+    df_hour = pd.to_datetime(data["start_time"]).dt.hour
+    print(type(data))
+    print(type(df_hour))
+    df_hour[0]
+    df_day = pd.to_datetime(data["start_time"]).dt.day_name()
+    df_month = pd.to_datetime(data["start_time"]).dt.month_name()
+    print((df_hour[3]))
+    for i in range(len(data)):
+        if 0 == df_hour[i]:
+            print("it is")
+            break
+        if i == len(data) - 1:
+            print("it is not")
+    # dayCategoricals = pd.get_dummies(df_hour)
+    
+    data["isNight"] = df_hour.isin(range(0,6))
+    data["isMorning"] = df_hour.isin(range(12,6))
+    data["isDay"] = df_hour.isin(range(12,18))
+    data["isEvening"] = df_hour.isin(range(18,24))
+    data["isWeekend"] = df_day.isin(["Saturday", "Sunday"])
+    data["isWinter"] = df_month.isin(["December", "January", "February"])
+    data["isSpring"] = df_month.isin(["March", "April", "May"])
+    data["isSummer"] = df_month.isin(["June", "July", "August"])
+    data["isFall"] = df_month.isin(["September", "October", "November"])
+    
+    # print(data.head())
+    
+    return data
+
+def clamming(data, alpha):
+    lower = data["y"].quantile(alpha)#remove 1% of data where most gets removed from upper as plot shows more outlier data
+    upper = data["y"].quantile(1 - (0.01 - alpha))
+    data["y"].clip(lower, upper, inplace = True)
+    return data
+
+def get_imbalance_error(data):
+    df_minute = pd.to_datetime(data["start_time"]).dt.minute
+    # print(df_minute.head)
+    # for i in range(len(data)):
+    #     if 0 == df_minute[i]:
+    #         print("it is")
+    #         break
+    #     if i == len(data) - 1:
+    #         print("it is not")
+    data["o'clock"] = df_minute.isin([0])
+    # print(data["o'clock"][10:])
+    x = np.asarray(data.index[data["o'clock"] == True].tolist())
+    # print(type(x))
+    y = np.zeros(len(x))
+    for count, xi in enumerate(x):
+        y[count] = data["total"][xi]
+    x_nodes = np.zeros(len(x)+1)
+    y_vals = np.zeros(len(x)+1)
+    x_mid = (x[0:-1] + x[1:])/2
+    y_mid = y[0:-1]
+    x_nodes[1:-1] = x_mid
+    x_nodes[0] = 0
+    x_nodes[-1] = len(x) - 1#at last datapoint in data
+    y_vals[1:-1] = y_mid
+    y_vals[0] = data["total"][0]
+    y_vals[-1] = data["total"][len(data)-1]
+    
+    f = interp1d(x_mid, y_mid, kind = "cubic", fill_value = "extrapolate")
+    x_new = np.arange(0,len(data))
+    y_new = f(x_new)
+    error = np.asarray(data["total"]) - y_new
+    fig, ax = plt.subplots((2))
+    ax[0].set_title("first 100 points")
+    ax[0].plot(x_new[:100],data["total"][:100], label = "real")
+    ax[0].plot(x_new[:100], y_new[:100], label = "interpol")
+    ax[0].legend()
+    ax[1].plot(x_new[:100], error[:100], label = "error")
+    ax[1].legend()
+    
+    fig, ax = plt.subplots((2))
+    N = len(x_new)//2
+    ax[0].set_title("middle 100 points")
+    ax[0].plot(x_new[N:N + 100],data["total"][N:N + 100], label = "real")
+    ax[0].plot(x_new[N:N + 100], y_new[N:N + 100], label = "interpol")
+    ax[0].legend()
+    ax[1].plot(x_new[N:N + 100], error[N:N + 100], label = "error")
+    ax[1].legend()
+    
+    fig, ax = plt.subplots((2))
+    ax[0].set_title("last 100 points")
+    ax[0].plot(x_new[-100:],data["total"][-100:], label = "real")
+    ax[0].plot(x_new[-100:], y_new[-100:], label = "interpol")
+    ax[0].legend()
+    ax[1].plot(x_new[-100:], error[-100:], label = "error")
+    ax[1].legend()
+    
+    return error
+
+def create_dataset(data_train, data_val, n_seq, alpha = 0.005, add_features = [], alt = False):
+    ###Altering
+    
+    if alt:
+        training_error = get_imbalance_error(data_train)
+        val_error = get_imbalance_error(data_val)
+        print(f"training error type is {type(training_error)}")
+        print(training_error[100:110])
+        data_train["y"] = data_train["y"] - training_error
+        data_val["y"] = data_val["y"] - val_error
+        
+    ###Clamming
+    data_train = clamming(data_train, alpha)
+    data_val = clamming(data_train, alpha)
+    ###Adding features
+    if "time" in add_features:
+        data_train = add_time_features(data_train)
+        data_val = add_time_features(data_val)
+    else:
+        print("no time featurs added")
+        
+    if "lag" in add_features:
+        data_train = add_lag_features(data_train, all_lags = True)
+        data_val = add_lag_features(data_val, all_lags = True)
+    else:
+        if "naive" in add_features:
+            data_train = add_lag_features(data_train, naive = True)
+            data_val = add_lag_features(data_val, naive = True)
+            print("no features added at all, including y_prev")
+        else:
+            data_train = add_lag_features(data_train, all_lags = False)
+            data_val = add_lag_features(data_val, all_lags = False)
+        print("no additional lag features added")
+    data_train = data_train.drop(["start_time"], axis=1)
+    data_val = data_val.drop(["start_time"], axis=1)
+    
+    feature_list = data_train.columns
+
+    ###Normalizing 
+    # data, norm = preprocessing.normalize(np.asarray(data.drop(["start_time"], axis=1)).astype(np.float32), return_norm = True, axis = 0)
+
+    scaler = preprocessing.StandardScaler()
+    data_train = scaler.fit_transform(data_train)
+    data_val = scaler.transform(data_val)
+    
+    #y_data = smooth_third_deviation(y_data)
+    x_data_train = data_train[:,:-1]
+    y_data_train = data_train[:,[-1]]
+    x_data_val = data_val[:,:-1]
+    y_data_val = data_val[:,[-1]]
+    
+    
+    x_list_train, y_list_train = [], []
+    x_list_val, y_list_val = [], []
+    
+    for i in range(len(x_data_train) - n_seq - 1):
+        x_list_train.append(x_data_train[i:(i+n_seq)]) #n_seq long inputs up to but not included i + n_seq. This includes y_prevs up tothe 5 minutes before
+        y_list_train.append(y_data_train[(i+n_seq)]) #The imbalance y estimated at t = t_{i+n_seq}
+    for i in range(len(x_data_val) - n_seq - 1):
+        x_list_val.append(x_data_val[i:(i+n_seq)])
+        y_list_val.append(y_data_val[(i+n_seq)])
+    if alt:
+        return np.array(x_list_train), np.array(y_list_train), np.array(x_list_val), np.array(y_list_val), feature_list, training_error, val_error
+    return np.array(x_list_train), np.array(y_list_train), np.array(x_list_val), np.array(y_list_val), feature_list
+
+# struct_imb(df_train,-80,-1)
+
+# def struct_imb2(data, x0,x1):
--- A3/ClassRNN.py
+++ A3/ClassRNN.py
@@ -14,47 +14,101 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import keras.backend as K
 
+class LossHistory(keras.callbacks.Callback):
+        def on_train_begin(self, logs={}):
+            self.history = {'loss':[],'val_loss':[]}
+
+        def on_batch_end(self, batch, logs={}):
+            self.history['loss'].append(logs.get('loss'))
+
+        def on_epoch_end(self, epoch, logs={}):
+            self.history['val_loss'].append(logs.get('val_loss'))
+            
+            
 class RNN:
-    def __init__(self, n_seq, n_dim, trained = False, filename = "Test"):
+    def __init__(self, n_seq, n_dim, trained = False, filename = "Test", num_LSTM = 64, lr = 0.00001, dropout = 0, double = False, triple = False, reg_val = 0):
         self.n_seq  = n_seq
         self.n_dim = n_dim
         self.filename = filename
+        self.num_LSTM = num_LSTM
+        self.lr = lr
     
-    
+        
         Input = keras.Input(shape = (self.n_seq, self.n_dim), name='Input') # [n_batch, n_seq, n_dim]
         print("input shape is: ", Input.shape)
-        
-        x = layers.LSTM(32, activation = "relu", return_sequences=True, name = "LSTM_unit_1")(Input)
-        Output = layers.Dense(1, activation = "linear")(x) #output should be unbounded
-        
+        #x = CuDNNLSTM(self.num_LSTM, kernel_initializer='glorot_uniform', recurrent_initializer='glorot_uniform', return_sequences=False)(Input)
+        x = layers.LSTM(self.num_LSTM, activation = "tanh", return_sequences = double, name = "LSTM_unit_1", kernel_regularizer=l2(reg_val))(Input)
+        if dropout:
+            x = layers.Dropout(dropout,name = "dropout_unit_1")(x)
+        if double:
+            x = layers.LSTM(self.num_LSTM, activation = "tanh", return_sequences = triple, name = "LSTM_unit_2", kernel_regularizer=l2(reg_val))(x)
+            if triple:
+                if dropout:
+                    x = layers.Dropout(dropout,name = "dropout_unit_2")(x)
+                x = layers.LSTM(self.num_LSTM, activation = "tanh", return_sequences = False, name = "LSTM_unit_3", kernel_regularizer=l2(reg_val))(x)
+        Output = layers.Dense(1)(x) #output should be unbounded
         self.Model = models.Model(Input, Output, name='RNN')
         
         
-        self.optim = keras.optimizers.Adam(learning_rate = 0.0001)
+        self.optim = keras.optimizers.Adam(learning_rate = self.lr)
         self.loss = keras.losses.MeanSquaredError()
         
         self.Model.compile(optimizer = self.optim, loss = self.loss)
         self.Model.summary()
         self.trained = trained
-    
-    
-    def train(self, x_train, y_train, x_val, y_val, batch_size = 1024, epochs = 5):
+        if trained:
+            self.Model.load_weights(filename)
+            print("weights loaded")
+        
+    def train(self, x_train, y_train, x_val, y_val, batch_size = 32, epochs = 5):
         if not self.trained:
-            
+            history = LossHistory()
             self.Model.fit(
                 x_train,
                 y_train,
                 epochs = epochs,
                 shuffle = True,
                 batch_size = batch_size,
-                validation_data=(x_val, y_val)
+                validation_data=(x_val, y_val),
+                callbacks=[history]
                 )
             
             self.Model.save_weights(self.filename)
             print("model is trained and weights saved")
+            return history
         else:
             print("model is already trained")
+            return None
+    # def get_history(self):
+    #     with open(file=self.history_filename, mode = "rb") as file:
+    #         data = pickle.load(file)
+    #     return data
             
     def predict(self, data):
         return self.Model.predict(data)
-            
+    
+    def n_in_1_out(self, data, pred_window, feature_list, start_ind = False): #data is assumed to be of the [batch, n_seq, n_feature] format
+        y_prev_loc = feature_list.get_loc("y_prev")
+        print(f"u_prev_loc is : {y_prev_loc}")
+        if not start_ind:
+            start_ind = len(data) - pred_window
+        else:
+            if start_ind > len(data) - pred_window:
+                print("start index to high")
+                return np.ones(pred_window)
+        forecasts = []
+        print("datashape is: ", data.shape)
+        model_input = data[[start_ind]]
+        print("model input shape is:" ,model_input.shape)
+        forecast = self.Model.predict(model_input)
+        print("forecast shape is: ", forecast.shape)
+        forecasts.append(forecast)
+        for i in range(pred_window - 1):
+            for j in range(1, i+2): #take i+1 last element of datapoint start index + i and replace y_prev with y_forecast
+                data[start_ind + 1 + i, -j, y_prev_loc] = forecasts[-j] #Takes the last y_prev and replaces it with the forecast in place on data
+            model_input = data[[start_ind + 1 + i]] #note that the other lag features are not changed, so predictions over 24 hours will not be valid
+            #print(model_input.shape)
+            #model_input[0, -1, -1] = forecasts[-1] #Last prev_y datapoint is replaced by last prediction
+            forecast = self.Model.predict(model_input)
+            forecasts.append(forecast)
+        return np.array(forecasts).copy(), start_ind
